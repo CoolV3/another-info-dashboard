ab37258093faa9f813c6e26eb8b0f18b1129b47d
CoolV3 <meierconsti@web.de>
2026-08-20 15:35:36 +0200

    Added basic speech of the day.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import ClockComponent from "@/components/clock";
+import SpeedOfDayComponent from "@/components/speechoftheday";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col gap-30 flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <ClockComponent/>
+        <SpeedOfDayComponent/>
+
     </div>
   );
 }
---
+++ components/speechoftheday.tsx
@@ -0,0 +1,14 @@
+import speechofday from "@/lib/speechofday.json"
+
+
+export default function SpeedOfDayComponent() {
+
+
+
+    return (
+        <div>
+            <p className="text-2xl font-bold">Speech of the day:</p>
+            <p className="text-3xl">Live isn´t always right, important </p>
+        </div>
+    )
+}
